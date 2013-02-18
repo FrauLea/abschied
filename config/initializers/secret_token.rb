41a1956d5be3473a79ec6b7e5fa38d3d0e079a60
FrauLea <Lea.Gscheidel@gmail.com>
2013-02-18 20:18:04 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abschied::Application.config.secret_token = '747b3ef73aea27e9d1a30699ce4bec12389493fe48bc9f6cfe33ecfd6b3dc092af3d2d40ed23c029186531b84f873999e22dc8f08c67fa7f8e2fba37eb06d4e4'
